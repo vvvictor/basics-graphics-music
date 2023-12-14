61046027ee1ebac2dd24eeb867ab5b54b8cd7bef
vvvictor <vvyasovtsev@mail.ru>
2023-12-14 18:39:28 +0500

    sss

--- boards/tang_nano_9k/board_specific_top.sv
+++ boards/tang_nano_9k/board_specific_top.sv
@@ -1,6 +1,9 @@
 `include "config.svh"
 `include "lab_specific_config.svh"
 
+`undef ENABLE_TM1638
+`define ENABLE_HDMI
+
 module board_specific_top
 # (
     parameter   clk_mhz = 27,
@@ -27,6 +30,11 @@ module board_specific_top
     output [              3:0]  VGA_G,
     output [              3:0]  VGA_B,
 
+    output       tmds_clk_n,
+    output       tmds_clk_p,
+    output [2:0] tmds_d_n,
+    output [2:0] tmds_d_p,
+    
     inout  [w_gpio      - 1:0]  GPIO
 );
 
@@ -57,6 +65,16 @@ module board_specific_top
 
     `endif
 
+    `ifdef ENABLE_HDMI
+        localparam w_top_vgar  = 8 
+                 , w_top_vgag  = 8
+                 , w_top_vgab  = 8; 
+    `else
+        localparam w_top_vgar  = 4
+                 , w_top_vgag  = 4
+                 , w_top_vgab  = 4; 
+    `endif
+
     //------------------------------------------------------------------------
 
     wire  [w_tm_key    - 1:0] tm_key;
@@ -70,6 +88,12 @@ module board_specific_top
     wire                      rst;
     wire  [              7:0] abcdefgh;
     wire  [             23:0] mic;
+
+    wire                      vga_hs;
+    wire                      vga_vs;
+    wire  [ w_top_vgar - 1:0] vga_r;
+    wire  [ w_top_vgag - 1:0] vga_g;
+    wire  [ w_top_vgab - 1:0] vga_b;
 
    //------------------------------------------------------------------------
 
@@ -90,6 +114,8 @@ module board_specific_top
 
     `endif
 
+
+
     //------------------------------------------------------------------------
 
     wire slow_clk;
@@ -107,6 +133,11 @@ module board_specific_top
         .w_led   ( w_top_led     ),
         .w_digit ( w_top_digit   ),
         .w_gpio  ( w_gpio    )
+     
+      , .w_vgar  ( w_top_vgar )
+      , .w_vgag  ( w_top_vgag )
+      , .w_vgab  ( w_top_vgab )
+
     )
     i_top
     (
@@ -122,12 +153,12 @@ module board_specific_top
         .abcdefgh ( abcdefgh  ),
         .digit    ( top_digit ),
 
-        .vsync    ( VGA_VS    ),
-        .hsync    ( VGA_HS    ),
+        .vsync    ( vga_hs    ),
+        .hsync    ( vga_vs    ),
 
-        .red      ( VGA_R     ),
-        .green    ( VGA_G     ),
-        .blue     ( VGA_B     ),
+        .red      ( vga_r     ),
+        .green    ( vga_g     ),
+        .blue     ( vga_b     ),
 
         .mic      ( mic       ),
         .gpio     (           )
@@ -148,7 +179,7 @@ module board_specific_top
 
     //------------------------------------------------------------------------
 
-    /*tm1638_board_controller
+    tm1638_board_controller
     # (
         .w_digit ( w_tm_digit )
     )
@@ -163,7 +194,7 @@ module board_specific_top
         .sio_clk  ( GPIO [0]  ),
         .sio_stb  ( GPIO [1]  ),
         .sio_data ( GPIO [2]  )
-    );*/
+    );
 
     //------------------------------------------------------------------------
 
@@ -177,6 +208,49 @@ module board_specific_top
         .sd    ( GPIO [6] ),
         .value ( mic      )
     );
+
+
+    `ifdef ENABLE_HDMI
+      wire clk_hd;
+      wire clk_px;
+
+      Gowin_rPLL hdPLL(
+        .clkout( clk_hd ), //output clkout
+        .clkin ( clk    ) //input clkin
+      );   
+
+      Gowin_CLKDIV pxCLK(
+        .clkout(clk_px), //output clkout
+        .hclkin(clk_hd), //input hclkin
+        .resetn( ~ rst) //input resetn
+      );
+
+
+//		.I_rgb_de(I_rgb_de_i), //input I_rgb_de
+	  DVI_TX_Top DVImod(
+		.I_rst_n( ~ rst), //input I_rst_n
+		.I_serial_clk( clk_hd ), //input I_serial_clk
+		.I_rgb_clk( clk_px ), //input I_rgb_clk
+		.I_rgb_vs(vga_vs), //input I_rgb_vs
+		.I_rgb_hs(vga_hs), //input I_rgb_hs
+
+		.I_rgb_de( 1'b1 ), //input I_rgb_de
+
+		.I_rgb_r(vga_r), //input [7:0] I_rgb_r
+		.I_rgb_g(vga_g), //input [7:0] I_rgb_g
+		.I_rgb_b(vga_b), //input [7:0] I_rgb_b
+		.O_tmds_clk_p(tmds_clk_p), //output O_tmds_clk_p
+		.O_tmds_clk_n(tmds_clk_n), //output O_tmds_clk_n
+		.O_tmds_data_p(tmds_d_p), //output [2:0] O_tmds_data_p
+		.O_tmds_data_n(tmds_d_n) //output [2:0] O_tmds_data_n
+	);
+    `else
+       assign VGA_HS = vga_hs;
+       assign VGA_VS = vga_vs;
+       assign VGA_R  = vga_r;
+       assign VGA_G  = vga_g;
+       assign VGA_B  = vga_b;
+    `endif
 
     assign GPIO [8] = 1'b0;  // GND
     assign GPIO [7] = 1'b1;  // VCC
